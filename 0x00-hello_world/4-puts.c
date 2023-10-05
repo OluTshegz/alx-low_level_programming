0c4492c5c9ab671e705349a19e884eb449007f6a
Olusegun <ayosegun2001@gmail.com>
2023-10-05 11:27:51 -0700

    print some string of characters followed by a new line with no use of printf, rather puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: Program that prints a statement without using printf
  * return: 0 if successful
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
