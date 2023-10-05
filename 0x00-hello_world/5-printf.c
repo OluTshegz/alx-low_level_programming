278716a6b941f767202df10990c269ad17bb54dd
Olusegun <ayosegun2001@gmail.com>
2023-10-05 11:55:12 -0700

    print some string of characters followed by a new line with no use of puts, rather printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /*
  * main: Entry point
- * Description: Program that prints a statement without using puts but instead using printf
+ *
+ * Description: Program that prints a statement without using puts
+ * 
  * Return: 0 if successful
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
+
 	return (0);
 }
