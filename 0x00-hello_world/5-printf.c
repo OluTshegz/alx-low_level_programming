77be87ceca7c1638277cfde557e9d3c24d1949c6
Olusegun <ayosegun2001@gmail.com>
2023-10-05 11:57:53 -0700

    print some string of characters followed by a new line with no use of puts, rather printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
  * main: Entry point
  *
@@ -6,6 +7,7 @@
  * 
  * Return: 0 if successful
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
