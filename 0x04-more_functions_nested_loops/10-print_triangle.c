64b398014b73abd9be7be3b71d21a14c0a21c92b
Olusegun <ayosegun2001@gmail.com>
2023-10-12 12:03:28 -0700

    ..

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,25 @@
 
 void print_triangle(int size)
 {
-	int row, column, i;
-
 	if (size <= 0)
-		_putchar('\n');
-
-	for (row = 0; row < size; row++)
 	{
-		for (column = size - row; column > 1; column++)
-		{
-			_putchar(' ');
-		}
-		for (i = row + column; i >= 1; i--)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		int i, x;
+		
+		for (i = 1; i <= size; i++)
+		{
+			for (x = 1; x < size; x++)
+			{
+				_putchar(' ');
+			}
+			for (x = 1; x <= 1; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
