4f66d5cea28e0114c11d00b0384f21011b784727
Olusegun <ayosegun2001@gmail.com>
2023-10-12 10:35:24 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int row, column, i;
+	
+	if (size <= 0)
+		_putchar('\n');
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column++)
+		{
+			_putchar(' ');
+		}
+		for (i = row + column; i >= 1; i--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar(char);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +8,5 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
