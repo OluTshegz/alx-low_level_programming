b2da89ca27af51ff306aa31da740c4c5354dcc5e
Olusegun <ayosegun2001@gmail.com>
2023-10-27 11:21:17 -0700

    Neither irony nor sarcasm is argument: Write a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int x, y, mul;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+
+	printf("%d\n", mul);
+	return (0);
+}
