bc7e7f995fa7d6a01d5adac20ff5bcdc50b13e56
Olusegun <ayosegun2001@gmail.com>
2023-10-31 09:33:03 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * *str_concat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: NULL on failure or a pointer that should point to
- * a newly allocated space in memory which contains the contents of s1,
- * followed by the contents of s2, and null terminated
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int x = 0, y = 0, i = 0, j = 0;
+	unsigned int x, y, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	x = 0;
-	while (s1[x] != '\0')
-	{
-		x++;
-	}
-	y = 0;
-	while (s2[y] != '\0')
-	{
-		y++;
-	}
-	s3 = malloc(sizeof(char) * (x + y + 1));
+	while (s1 && s1[len1])
+		len1++;
+
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
-		return (0);
-	for (i = 0; i < x; i++)
+		return (NULL);
+	x = 0;
+	y = 0;
+
+	if (s1)
 	{
-		if (*s1)
-			s3[i] = s1[i];
+		while (x < len1)
+		{
+			s3[x] = s1[x];
+			x++;
+		}
 	}
-	for (j = 0; j < y; j++)
+
+	if (s2)
 	{
-		if (*s2)
-			*(s3 + j + j) = s2[j];
+		while (x < (len1 + len2))
+		{
+			s3[x] = s2[y];
+			x++;
+			y++;
+		}
 	}
+	s3[x] = 0;
+
 	return (s3);
 }
