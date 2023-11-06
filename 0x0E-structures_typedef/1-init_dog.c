b590e029beb57fa26b0e216599f3a9b35e4676ed
Olusegun <ayosegun2001@gmail.com>
2023-11-06 10:51:30 -0800

    1. A dog is the only thing on earth that loves you more than you love yourself: Write a function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type structure dog
+ * @d: initializes pointer to structure dog
+ * @name: pointer to the name of the dog to initialize
+ * @age: age of dog to initialize
+ * @owner: pointer to owner of dog to initialize
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		d->age = age;
+		(*d).owner = owner;
+	}
+}
