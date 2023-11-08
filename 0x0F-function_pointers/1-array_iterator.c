e4f6d6262e9636666792c3ebe1d5014495eb6509
Olusegun <ayosegun2001@gmail.com>
2023-11-08 09:17:04 -0800

    1. If you spend too much time thinking about a thing, you'll never get it done: Write a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to function used
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	else
+	{
+		while (size > 0)
+		{
+			action(*array);
+			array++;
+			size--;
+		}
+	}
+}
