a14e2f78bed0537ff66e3d5dd093ecaf3714d96a
Olusegun <ayosegun2001@gmail.com>
2023-11-22 10:58:46 -0800

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,25 +10,44 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+	const listint_t *slow_ptr, *fast_ptr;
+	size_t count;
 	long int difference;
 
-	if (head == NULL)
-		exit(98);
+	count = 0;
+	slow_ptr = head;
+	fast_ptr = head;
+	while (head == NULL)
+		return (0);
 
 	while (head)
 	{
 		difference = (head) - (head->next);
 		count++;
 		printf("[%p] %d\n", (void *)head, head->n);
-
-		if (difference < 0)
+		if (difference > 0)
+			head = head->next;
+		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-		else
-			head = head->next;
+	}
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+		count += 1;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		/* Check for a loop */
+		if (slow_ptr == fast_ptr)
+			exit(98);
+	}
+	/* Print the last node if it exists and is not part of the loop */
+	if (slow_ptr != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+		count++;
 	}
 	return (count);
 }
