d2e7b05070b68659ff666758f55ce642da71728b
Olusegun <ayosegun2001@gmail.com>
2024-01-15 19:25:17 +0100

    Task 3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/gm_win.c
+++ 0x18-dynamic_libraries/gm_win.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * printf - prints formatted characters,
+ * a variadic function, takes multiple parameters/arguments
+ * @format: pointer to the formatted characters
+ *
+ * Return: 0 if successful, -1 or 1 if unsuccessful
+*/
+
 int printf(const char *format, ...)
 {
-    write(1, "9 8 10 24 75 - 9\n", 17);
-    write(1, "Congratulations, you win the Jackpot!\n", 38);
-    exit(EXIT_SUCCESS);
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
