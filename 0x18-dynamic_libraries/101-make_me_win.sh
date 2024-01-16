0ff36b1013036f3d8670e7338e5fac0697cd0c77
Olusegun <ayosegun2001@gmail.com>
2024-01-16 12:04:01 +0100

    Task 3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/OluTshegz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm_win.so
+wget -P /tmp/ https://raw.githubusercontent.com/OluTshegz/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/gm_win.so
