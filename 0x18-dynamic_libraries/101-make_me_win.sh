ad1665090b3129780488ff77a2b5cd2550ed8604
Olusegun <ayosegun2001@gmail.com>
2024-01-18 18:23:08 +0100

    updated script file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/OluTshegz/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp/ https://raw.githubusercontent.com/OluTshegz/alx-low_level_programming/master/0x18-dynamic_libraries/gm_win.so
 export LD_PRELOAD=/tmp/gm_win.so
