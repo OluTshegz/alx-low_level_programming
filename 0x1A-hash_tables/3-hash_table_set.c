68da0aea004d22f828a3ec2a60154f9ea0039488
Olusegun <ayosegun2001@gmail.com>
2024-02-02 17:29:11 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,28 +6,21 @@
  * @value: The value associated with key
  * Return: 0 for error/failure, otherwise 1 for success
  */
-/**
- * Task 3. >>> ht['betty'] = 'cool':
- * a function that adds an element to the hash table.
- */
+/*Task 3. >>> ht['betty'] = 'cool':function adds element to hash table.*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	char *value_copy;
 	unsigned long int index, i;
-
 	/* If the hash table, key, or value is NULL, return 0. */
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
-	{
-		/* If the key already exists, update the value and return 1. */
+	{/* If the key already exists, update the value and return 1. */
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
@@ -36,7 +29,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	/**
-     * If the key doesn’t exist, create a new node and add it to the hash table. */
+     * If the key doesn’t exist, create
+     * a new node and add it to the hash table.
+    */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
